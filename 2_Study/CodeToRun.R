8dd13f3e050ba5f8031ddb7ccc3ce66dbd9c4992
daniellenewby <danielleanne.newby@gmail.com>
2024-10-30 12:58:00 +0000

    tidy shiny

--- 2_Study/CodeToRun.R
+++ 2_Study/CodeToRun.R
@@ -89,14 +89,9 @@ study_start <- "..."
 
 # Run the study ------
 # For some data partners they may not have be able to run certain studies i.e. a cancer registry
-# will not have a background denominator population therefore run_incidence/run_prevalence will be set to FALSE also
-# if a data partner does not have suitable mortality data then survival analysis will also be set to FALSE
+# will not have a background denominator population therefore run_incidence will be set to FALSE
 run_incidence <- TRUE
 run_characterisation <- TRUE
-
-# extra's if a data partner has approval
-run_prevalence <- FALSE
-run_survival <- FALSE
 
 source(here("RunStudy.R"))
 
